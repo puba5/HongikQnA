7810199332f2198576a525b53a44e706489e6b7e
w-son <williamson@naver.com>
2019-08-25 00:06:04 +0900

    2019-08-25 00:06

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# 생성한 모델은 2가지 단계, 
+# 1. migration이 행해져야 하고    2. admin.py에 등록해야한다
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
