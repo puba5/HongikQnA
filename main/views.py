2550a5d4638e1269274d917026acec27256fa67c
w-son <williamson@naver.com>
2019-08-24 23:01:23 +0900

    2019-08-24 23:01

--- main/views.py
+++ main/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def start(request):
     return render(request, 'main/start.html')
+
+    # main 어플리케이션 아래에는 templates/main/ 위치 아래에 html 파일이,  static/main/ 위치 아래에 css 파일이 있다
+    
+    # view에서 html파일, 그리고 html에서 css파일로 연결이 된다
