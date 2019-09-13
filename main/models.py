78f18e93e94684b1f4a17e4e12d5f7ddd3d84daf
w-son <williamson@naver.com>
2019-09-14 00:32:50 +0900

    2019-09-14 post 모델 수정, 작성 시간 추가

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # 생성한 모델은 2가지 단계, 
 # 1. migration이 행해져야 하고    2. admin.py에 등록해야한다
@@ -7,5 +8,6 @@ class Post(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=100)
     body = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__ (self):
         return self.title
